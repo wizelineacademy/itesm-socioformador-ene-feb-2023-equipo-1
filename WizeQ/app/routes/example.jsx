65d098b639ba764ff19c9940fac675214a37bcc2
Bren Saucedo <88901374+BrendaSaucedo12@users.noreply.github.com>
2023-04-20 19:24:11 -0600

    chatbot-box-created

--- WizeQ/app/routes/example.jsx
+++ WizeQ/app/routes/example.jsx
@@ -1,70 +1,154 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 // import userImg from 'build/_assets/placeholder_user_img-ZWAQNLBE.png'
-import botImage from 'build/_assets/logo_answerbot.png'
+// import botImg from 'build/_assets/logo_answerbot.png'
 
-const BotButton = styled.button`
+const ChatbotContainer = styled.div`
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  width: 330px;
+  height: 430px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+  background-color: #E1EAF4;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ChatbotHeader = styled.div`
   background-color: #213246;
-  border: none;
-  width: 150px;
-  height: 150px;
-  margin: 75% 0 0 0;
-  float: right;
-  border-top-left-radius: 25px;
-  border-bottom-left-radius: 25px;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.5);
-  position: relative;
-  transition: width 0.3s ease-in-out;
+  height: 40px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 60px;
+`;
 
-  & > .message {
-    opacity: 0;
-    visibility: hidden;
-    transition: opacity 0.3s ease-in-out;
-  }
+const IconBot = styled.img`
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background-color: #fff;
+  border: 0px solid transparent;
+  background-image: url('/build/_assets/logo_answerbot-6JNTGKON.png');
+  background-size: 75%;
+  background-repeat: no-repeat;
+  background-position: center;
+  box-shadow: inset 0 0 10px 0 rgba(0, 0, 0, 0.5);
+  position: absolute;
+  left: 5%;
+`
+
+const BotName = styled.text`
+  display: flex;
+  align-items: center;
+  font-weight: bold;
+  color: #fff;
+  padding: 0 80px;
+`
+
+const CloseButton = styled.button`
+  background-color: transparent;
+  background-image: url('/build/_assets/close-button-2.png');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  border: none;
+  width: 50px;
+  height: 50px;
 
   &:hover {
-    width: 360px;
+    background-size: 150%;
+    width: 50px;
+    height: 50px;
+  }
 
-    & > .message {
-      opacity: 1;
-      visibility: visible;
-      transition: opacity 1.1s ease-in-out;
-    }
+`
+
+const ChatbotMessages = styled.div`
+  flex: 1;
+  padding: 10px;
+  overflow-y: auto;
+`;
+
+const Message = styled.div`
+  padding: 5px 10px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  margin: 5px 0;
+  max-width: 80%;
+
+  &.user {
+    background-color: #ffffff;
+    border-bottom-left-radius: 10px;
+    align-self: flex-end;
+  }
+
+  &.bot {
+    background-color: #213246;
+    border-bottom-right-radius: 10px;
   }
 `;
 
-const BotIcon = styled.img`
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    background-color: #fff;
-    border: none;
-    background-image: url(${botImage});
-    background-size: 75%;
-    background-repeat: no-repeat;
-    background-position: center;
-    box-shadow: inset 0 0 10px 0 rgba(0, 0, 0, 0.5);
-    position: absolute;
-    left: 27.5px;
-    transform: translateY(-50%);
-`
+const ChatbotInput = styled.form`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+`;
 
-const BotMessage = styled.div`
-  color: #fff;
-  font-size: 20px;
-  font-weight: bold;
-  position: absolute;
-  left: 40%;
-  right: 5%;
-  transform: translateY(-50%);
+const Input = styled.input`
+  flex: 1;
+  padding: 5px;
+  border-radius: 5px;
+  border: none;
+  margin-right: 10px;
+`;
+
+const Button = styled.button`
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  background-color: #f0f0f0;
+  cursor: pointer;
 `;
 
 function Chatbot() {
+  const [messages, setMessages] = useState([]);
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    const input = e.target.querySelector('input');
+    const message = input.value;
+    setMessages([...messages, { text: message, user: true }]);
+    input.value = '';
+    // Aquí iría la lógica para procesar la entrada del usuario y generar una respuesta
+    // Por ejemplo, podrías usar una API de chatbot para obtener la respuesta
+    // Luego, agregarías la respuesta a la lista de mensajes usando setMessages
+  };
+
   return (
-    <BotButton>
-        <BotIcon/>
-        <BotMessage className="message"> Hi, I'm AnswerBot! Ask me anything! </BotMessage>
-    </BotButton>
+    <ChatbotContainer>
+      <ChatbotHeader>
+        <IconBot/>
+        <BotName> AnswerBot </BotName>
+        <CloseButton/>
+      </ChatbotHeader>
+      <ChatbotMessages>
+        {messages.map((message, index) => (
+          <Message key={index} className={message.user ? 'user' : 'bot'}>
+            {message.text}
+          </Message>
+        ))}
+      </ChatbotMessages>
+      <ChatbotInput onSubmit={handleInput}>
+        <Input type="text" placeholder="Escribe un mensaje..." />
+        <Button type="submit">Enviar</Button>
+      </ChatbotInput>
+    </ChatbotContainer>
   );
 }
 
