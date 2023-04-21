e3006cacfee0d14ce15128bf19a418dd8b839468
Bren Saucedo <88901374+BrendaSaucedo12@users.noreply.github.com>
2023-04-20 18:28:27 -0600

    Revert "AnswerBot-Button-Fixed"
    This reverts commit d20bdef8124844fbf72e2ace3ae8dddd77ebb0dd.

--- WizeQ/app/routes/questions/new.jsx
+++ WizeQ/app/routes/questions/new.jsx
@@ -14,7 +14,6 @@ import {
 import listDepartments from 'app/controllers/departments/list';
 import createQuestion from 'app/controllers/questions/create';
 import Notifications from 'app/components/Notifications';
-import AnswerBotButton from 'app/components/Atoms/AnswerBotButton';
 
 export const loader = async ({ request }) => {
   await requireAuth(request);
@@ -123,7 +122,6 @@ function CreateQuestion() {
               }
             </Styled.Recommendations>
           </Styled.RecommendationsContainer>
-          <AnswerBotButton/>
         </Styled.QuestionRecommendations>
       </Styled.QuestionDiv>
     </>
