966c84937b1468270d1b0d2e85d32413f27bc681
Bren Saucedo <88901374+BrendaSaucedo12@users.noreply.github.com>
2023-04-26 18:23:59 -0600

    updates
    - like/dislike buttons
    - renamed folder of bot

--- WizeQ/app/components/Atoms/AnswerBotButton/AnswerBotButton.jsx
+++ WizeQ/app/components/Atoms/AnswerBotButton/AnswerBotButton.jsx
@@ -4,7 +4,7 @@ import {pdfConv} from 'app/controllers/answerBot/pdfConv';
 
 function AnswerBotButton() {
 
-    const instructions = "Instructions: Compose a comprehensive reply to the query using the search results given.\nCite each reference using [number] notation (every result has this number at the beginning).\nCitation should be done at the end of each sentence. If the search results mention multiple subjects\nwith the same name, create separate answers for each. Only include information found in the results and\ndon't add any additional information. Make sure the answer is correct and don't output false content.\nIf the text does not relate to the query, simply state 'No se encontro respuesta'. Don't write 'Answer:'Directly start the answer.\n"
+    const instructions = "Instructions: Compose a comprehensive reply to the query using the search results given.\n If the search results mention multiple subjects\nwith the same name, create separate answers for each. Only include information found in the results and\ndon't add any additional information. Make sure the answer is correct and don't output false content.\nIf the text does not relate to the query, simply state 'Sorry, I couldn't find an answer to your question.'. Don't write 'Answer:'Directly start the answer.\n"
 
     const messagesEndRef = useRef(null);
 
@@ -43,6 +43,40 @@ function AnswerBotButton() {
         console.log(chatbotVisible);
     };
 
+    const [likedButtons, setLikedButtons] = useState({});
+    const [dislikedButtons, setDislikedButtons] = useState({});
+    const [showThanksMessage, setShowThanksMessage] = useState({});
+
+    const handleLikeClick = (index) => {
+        setLikedButtons((prevLikedButtons) => ({
+          ...prevLikedButtons,
+          [index]: !prevLikedButtons[index],
+        }));
+        setDislikedButtons((prevDislikedButtons) => ({
+          ...prevDislikedButtons,
+          [index]: false,
+        }));
+        setShowThanksMessage((prevShowThanksMessage) => ({
+            ...prevShowThanksMessage,
+            [index]: !likedButtons[index],
+        }));
+    };
+    
+    const handleDislikeClick = (index) => {
+        setDislikedButtons((prevDislikedButtons) => ({
+            ...prevDislikedButtons,
+            [index]: !prevDislikedButtons[index],
+        }));
+        setLikedButtons((prevLikedButtons) => ({
+            ...prevLikedButtons,
+            [index]: false,
+        }));
+        setShowThanksMessage((prevShowThanksMessage) => ({
+            ...prevShowThanksMessage,
+            [index]: !dislikedButtons[index],
+        }));
+    };
+
     return (
         <div>
             <Styled.BotButton visible={!chatbotVisible} onClick={handleChatbotToggle}>
@@ -62,23 +96,52 @@ function AnswerBotButton() {
                 </Styled.ChatbotHeader>
 
                 <Styled.ChatbotMessages>
-                {messages.slice(1).map((message, index) => (
+                {messages.map((message, index) => (
                     message.role == "user" ? 
                     <Styled.ChatbotRowMessage style={{justifyContent: 'flex-end'}}>
-                        <Styled.Message key={index} className='user' ref={messagesEndRef}> {message.content} </Styled.Message>
+                        <Styled.Message 
+                            key={index}
+                            className='user'
+                            ref={messagesEndRef}>
+                            {message.content}
+                        </Styled.Message>
                         <Styled.IconUser src='/build/_assets/placeholder_user_img-ZWAQNLBE.png'/> 
                     </Styled.ChatbotRowMessage>
                     :
-                    <Styled.ChatbotRowMessage style={{justifyContent: 'flex-start'}}>
-                        <Styled.IconBot/>
-                        <Styled.Message key={index} className='bot' ref={messagesEndRef}> {message.content} </Styled.Message>
-                    </Styled.ChatbotRowMessage>
+                    <div>
+                        <Styled.ChatbotRowMessage style={{justifyContent: 'flex-start'}}>
+                            <Styled.IconBot/>
+                            <Styled.Message
+                                key={index}
+                                className='bot'
+                                ref={messagesEndRef}>
+                                {message.content}
+                            </Styled.Message>
+                        </Styled.ChatbotRowMessage>
+                        {index !== 0 ?
+                            <div>
+                                <Styled.likeButton
+                                    key={`like-${index}`}
+                                    liked={likedButtons[index]}
+                                    onClick={() => handleLikeClick(index)}
+                                />
+                                <Styled.dislikeButton
+                                    key={`dislike-${index}`}
+                                    disliked={dislikedButtons[index]}
+                                    onClick={() => handleDislikeClick(index)}
+                                />
+                                { showThanksMessage[index] ? <span style={{padding: '5px'}}> Thanks for the feedback! </span> : <> </> }
+                            </div>
+                            :
+                            <> </>
+                        }
+                    </div>
                 ))}
                 </Styled.ChatbotMessages>
 
                 <Styled.ChatbotInput onSubmit={handleInput}>
                 <Styled.Input type="text" placeholder="Enter your question..." />
-                <Styled.Button type="submit"/>
+                <Styled.sendButton type="submit"/>
                 </Styled.ChatbotInput>
             </Styled.ChatbotContainer>
         </div>
