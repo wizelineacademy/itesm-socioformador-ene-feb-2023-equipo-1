75955ab81b5e8848390e5a4852f262b7e8c82ba2
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-07 15:28:44 -0600

    Removed Tensorflow from Python API

--- AnswerBotOfficial/pdf_conversation_gpt.py
+++ AnswerBotOfficial/pdf_conversation_gpt.py
@@ -1,23 +1,9 @@
-import urllib.request
-import tensorflow_hub as hub
-import fitz
 import re
-import numpy as np
+import fitz
 import openai
-import os
-from tqdm.auto import tqdm
-from sklearn.neighbors import NearestNeighbors
-import sys
-import json
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-
-
-openai.api_key = "********************"
-
-
-app = Flask(__name__)
-
+from tqdm.auto import tqdm
 
 def preprocess(text):
     '''
@@ -28,7 +14,6 @@ def preprocess(text):
     text = text.replace('\n', ' ')
     text = re.sub('\s+', ' ', text)
     return text
-
 
 def pdf_to_text(path, start_page=1, end_page=None):
     '''
@@ -43,27 +28,25 @@ def pdf_to_text(path, start_page=1, end_page=None):
     text_list = []
 
     for i in tqdm(range(start_page-1, end_page)):
-        text = doc.load_page(i).get_text("content")
+        text = doc.load_page(i).get_text("text")
         text = preprocess(text)
         text_list.append(text)
 
     doc.close()
     return text_list
 
-
 def text_to_chunks(texts, word_length=150, start_page=1):
     '''
     convert list of texts to smaller chunks of length `word_length`
     '''
     text_toks = [t.split(' ') for t in texts]
-    page_nums = []
     chunks = []
-    
+
     for idx, words in enumerate(text_toks):
         for i in range(0, len(words), word_length):
             chunk = words[i:i+word_length]
             if (i+word_length) > len(words) and (len(chunk) < word_length) and (
-                len(text_toks) != (idx+1)):
+                    len(text_toks) != (idx+1)):
                 text_toks[idx+1] = chunk + text_toks[idx+1]
                 continue
             chunk = ' '.join(chunk).strip()
@@ -71,86 +54,41 @@ def text_to_chunks(texts, word_length=150, start_page=1):
             chunks.append(chunk)
     return chunks
 
-
-class SemanticSearch:
-    
-    def __init__(self):
-        self.use = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
-        self.fitted = False
-    
-    
-    def fit(self, data, batch=1000, n_neighbors=5):
-        self.data = data
-        self.embeddings = self.get_text_embedding(data, batch=batch)
-        n_neighbors = min(n_neighbors, len(self.embeddings))
-        self.nn = NearestNeighbors(n_neighbors=n_neighbors)
-        self.nn.fit(self.embeddings)
-        self.fitted = True
-    
-    
-    def __call__(self, text, return_data=True):
-        inp_emb = self.use([text])
-        neighbors = self.nn.kneighbors(inp_emb, return_distance=False)[0]
-        
-        if return_data:
-            return [self.data[i] for i in neighbors]
-        else:
-            return neighbors
-    
-    
-    def get_text_embedding(self, texts, batch=1000):
-        embeddings = []
-        for i in tqdm(range(0, len(texts), batch)):
-            text_batch = texts[i:(i+batch)]
-            emb_batch = self.use(text_batch)
-            embeddings.append(emb_batch)
-        embeddings = np.vstack(embeddings)
-        return embeddings
-
-
-recommender = SemanticSearch()
-
-
-def load_recommender(path, start_page=1):
-    global recommender
-    texts = pdf_to_text(path, start_page=start_page)
-    chunks = text_to_chunks(texts, start_page=start_page)
-    recommender.fit(chunks)
-    return 'Corpus Loaded.'
-
-
-def generate_text(messages, model="gpt-3.5-turbo"):
+def generate_answer(conversation, model="gpt-3.5-turbo"):
+    conversation.insert(0, pdf_context)
     completion = openai.ChatCompletion.create(
         model=model,
-        messages=messages
+        messages=conversation,
+        temperature=0.2
     )
     message = completion.choices[0].message
     return message
 
+def loadPDF(pdf_context, start_page=1):
+    texts = pdf_to_text("corpus.pdf", start_page=start_page)
+    chunks = text_to_chunks(texts, start_page=start_page)
+    pdf_context["content"] = "Context chunks: "
+    for c in chunks:
+        pdf_context["content"] += c
+    return pdf_context
 
-def generate_answer(conversation):
-    load_recommender('corpus.pdf')
-    topn_chunks = recommender(conversation[-1]["content"])
-    userInput = conversation[-1]["content"]
-    prompt = ""
-    prompt += 'search results:\n\n'
-    for c in topn_chunks:
-        prompt += c + '\n\n'
-    
-    prompt += f"Query: {userInput}\n\n"
-    conversation[-1]["content"] = prompt
-    answer = generate_text(conversation)
-    return answer
+# ========================================= MAIN =========================================
 
+openai.api_key = "*****************************"
 
-@app.route('/api/pdf_conversation_gpt', methods=['POST'])
+application = Flask(__name__)
+
+pdf_context = {"role": "system", "content": ""}
+pdf_context = loadPDF(pdf_context)
+print("PDF Loaded...")
+
+@application.route('/api/pdf_conversation_gpt', methods=['POST'])
 def submit_conversation():
     conversation = request.json
-    print(conversation)
     conversation.append(generate_answer(conversation))
     print(conversation[-1]["content"])
     return jsonify(conversation)
 
-CORS(app, origins='http://localhost:3000')
+CORS(application, origins='http://localhost:3000')
 if __name__ == '__main__':
-    app.run(port=3000,debug=True)
+    application.run(port=3000,debug=True)
