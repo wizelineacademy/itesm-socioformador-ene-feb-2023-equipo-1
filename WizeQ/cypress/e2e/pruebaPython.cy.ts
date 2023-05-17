535a296aedbab9ddb4f6373f0e99c1fcac010a53
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-17 12:19:11 -0600

    Successful tests: CP001, 3, 4, 5, 6, 8

--- WizeQ/cypress/e2e/pruebaPython.cy.ts
+++
@@ -1,8 +0,0 @@
-import pdfConv from 'app/controllers/answerBot/pdfConv'
-
-describe('empty spec', () => {
-  it('passes', () => {
-    //cy.visit('http://localhost:3000/questions/new')
-    cy.visit('http://localhost:3000/')
-  })
-})
--- WizeQ/tests/unit/CP001-abrirChat.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from 'remix';
-import { act } from 'react-dom/test-utils';
-import AnswerBotButton from 'app/components/Atoms/AnswerBotButton';
-
-describe('Testing pdfConv component', () => {
-  it('Test question that should have expected answer', () => {
-    render(<AnswerBotButton />);
-  });
-});
