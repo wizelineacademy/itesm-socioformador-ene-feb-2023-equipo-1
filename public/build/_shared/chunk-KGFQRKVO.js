e129b3e8f4b03e16a103fadda2353b9488b6f43c
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-18 15:09:23 -0600

    Merge test with ci-testing

---
+++ public/build/_shared/chunk-KGFQRKVO.js
@@ -0,0 +1,1 @@
+import{n as e}from"/build/_shared/chunk-FTDMWGVY.js";import{h as t}from"/build/_shared/chunk-MWCUBRQI.js";t();function c(n){return e().find(o=>o.id===n)}var f=c;export{f as a};
---
+++ public/build/routes/admin-IRJ2O6MH.js
@@ -0,0 +1,308 @@
+import{a as oe}from"/build/_shared/chunk-Z3SD4FDF.js";import{a as W}from"/build/_shared/chunk-6LFOK6VO.js";import{a as ee}from"/build/_shared/chunk-CWYXRMSG.js";import{a as te}from"/build/_shared/chunk-H4FXTFXP.js";import{e as Q,k as V,l as Y}from"/build/_shared/chunk-UZM5IF2K.js";import{d as R,g}from"/build/_shared/chunk-5GEAYPZ3.js";import"/build/_shared/chunk-MF2UCHQU.js";import"/build/_shared/chunk-KGFQRKVO.js";import{Aa as D,Ba as $,Ga as X,a as z,ha as Z}from"/build/_shared/chunk-GHFYQP6C.js";import{a as Ie}from"/build/_shared/chunk-I4IDDGZI.js";import{c as n}from"/build/_shared/chunk-NSPDEEEW.js";import{e as U,l as K,o as L,p as O}from"/build/_shared/chunk-FTDMWGVY.js";import{f,h as i,i as j}from"/build/_shared/chunk-MWCUBRQI.js";i();i();var x=f(j());var Me=f(Ie());i();var ne=n.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin: 40px auto;
+  width: 90vw;
+
+  @media (max-width: 576px) {
+    flex-direction: column;
+
+    h2 {
+      margin-bottom: 30px;
+    }
+  }
+`;i();i();var e=f(j()),b=f(z());var ie="/build/_assets/logomark_medium-U2FGMSIX.png";i();var re=f(j()),S="...",q=(o,r)=>{let p=r-o;return Array.from({length:p},(l,d)=>d+o)};function ae({currentPage:o,totalPages:r}){return(0,re.useMemo)(()=>{let l=r,d=1;if(2+d>l)return q(1,l);let s=Math.max(o-d,1),P=Math.min(o+d,l),_=s>2,N=P<l-2,A=1,y=l;if(!_&&N){let w=5*d;return[...q(1,w),S,l]}if(_&&!N){let w=5*d,I=q(l-w,l+1);return[A,S,...I]}let T=q(s,P+1);return[A,S,...T,S,y]},[o,r])}i();var le=n.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 2rem auto;
+  width: 90vw;
+  select {
+    width: 10rem;
+    border-radius: 5px;
+    background-color: #fff;
+    border: 1px solid #d3d3d4;
+    border-radius: 15px;
+    padding: 1px 4px;
+    margin: 1rem 0;
+    &:focus {
+      outline: none;
+    }
+  }
+`,M=n.table`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: 2rem auto;
+  border-radius: 9px;
+  border: 1px solid #f2efed;
+  tr {
+    justify-content: space-between;
+  }
+  @media (max-width: 481px) {
+    .table-desktop-view {
+      display: none;
+    }
+  }
+`,de=n.tr`
+  padding: 0.5rem 0;
+  border: 1px solid #f2efed;
+  .row-btn {
+    margin: 0;
+  }
+  td {
+    min-width: 18vw;
+    max-width: 18.01vw;
+    text-align: left;
+    padding: 0 0.8rem;
+    overflow-wrap: break-word;
+    div {
+      margin: 0.3 auto;
+      display: flex;
+      align-items: center;
+    }
+  }
+  img {
+    margin: 0.8rem;
+    width: 2.5rem;
+    border-radius: 50%;
+  }
+  @media (max-width: 767px) {
+    td {
+      min-width: 17.5vw;
+      max-width: 17.51vw;
+      text-align: left;
+      padding: 0 0.8rem;
+      overflow-wrap: break-word;
+    }
+    div {
+      margin: 0.3 auto;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    img {
+      margin: 0.3rem;
+      width: 2rem;
+    }
+  }
+  @media (max-width: 481px) {
+    .table-desktop-view {
+      display: none;
+    }
+    td {
+      min-width: 45.5vw;
+      max-width: 45.51vw;
+      text-align: left;
+      padding: 0 0.8rem;
+      overflow-wrap: break-word;
+    }
+    div {
+      flex-direction: row;
+    }
+  }
+`,se=n.tr`
+  background-color: #f4f7f9;
+  padding: 1rem 0;
+  th {
+    font-size: 1.5rem;
+    min-width: 18vw;
+    max-width: 18.01vw;
+    color: #524f4e;
+    margin: 2rem;
+    padding: 0 0 0 1rem;
+    @media (max-width: 767px) {
+      margin: 0;
+      padding: 0;
+    }
+  }
+  .button-header{
+    padding: 0.5rem 0 0 0;
+  }
+  
+  @media (max-width: 481px) {
+    th {
+      min-width: 45.5vw;
+      max-width: 45.51vw;
+      text-align: left;
+      padding: 0 0.8rem;
+      overflow-wrap: break-word;
+    }
+  }
+`,pe=n.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0.5rem auto;
+  padding: 1.5rem 0;
+  width: 90vw;
+  align-items: center;
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
+`,Ve=n(Q)`
+  background: transparent;
+  border: none;
+  font-size: 1.5rem;
+  color: #524f4e;
+  align-items: center;
+  :hover {
+    background: transparent;
+  }
+`,v=n(V)`
+ background-color: white;  
+`,me=n.div`
+  display: flex;
+  color: var(--color-secondary-active);
+  align-items: center;
+  background-color: var(--color-secondary-light);
+  border-radius: 10px;
+  font-family: "Nunito";
+  font-size: 16px;
+  min-height: 50px;
+  margin: 15px auto;
+  padding: 6px 16px;
+  justify-content: center;
+  width: 50vw;
+  .message {
+    padding: 8px 15px;
+  }
+  @media (max-width: 576px) {
+    width: 90vw;
+  }
+`;i();var t=f(j()),m=f(z());i();var fe=n.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 100%;
+  height: 100%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1050;
+`,ce=n.div`
+  font-family: "Nunito", sans-serif;
+  font-size: 14px;
+  border-radius: 15px;
+  background-color: white;
+  max-width: 500px;
+  width: calc(100% - 40px);
+  margin: 20px;
+  padding: 15px 0;
+  h2 {
+    margin: 0;
+  }
+  h3 {
+    margin: 15px 0;
+  }
+  span {
+    margin: 6px 0;
+  }
+`,ue=n.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  border-bottom: 1px solid #d8d8d8;
+  padding: 15px 25px;
+  @media (max-width: 576px) {
+    flex-direction: column;
+    justify-content: center;
+  }
+`,xe=n.div`
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 100%;
+  @media (max-width: 576px) {
+    text-align: center;
+  }
+`,ge=n.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 15px 25px;
+`,he=n.div`
+  border-radius: 15px;
+  border: 1px solid #d8d8d8;
+  margin: 10px 0 0;
+`,C=n.ul`
+  border-top: ${o=>o.noBorder?"none":"1px solid #d8d8d8"};
+  list-style: none;
+  margin: 0;
+  padding: 15px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-around;
+  li {
+    max-width: 50%;
+    width: 75px;
+  }
+  input[type="text"] {
+    max-width: calc(100% - 4px);
+    padding: 5px 2px;
+  }
+`,be=n.div`
+  margin-top: 25px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  button {
+    margin-left: 10px;
+  }
+  @media (max-width: 576px) {
+    justify-content: center;
+  }
+`;function G({user:o,onClose:r}){let[p,l]=(0,t.useState)(o.is_admin),[d,u]=(0,t.useState)(o.job_title);return t.default.createElement(fe,null,t.default.createElement(K,{method:"post"},t.default.createElement(ce,null,t.default.createElement(ue,null,t.default.createElement(Y,{src:o.profile_picture,size:"extra big"}),t.default.createElement(xe,null,t.default.createElement("h2",null,o.full_name),t.default.createElement("span",null,t.default.createElement("b",null,o.job_title!==null?o.job_title:"")),t.default.createElement("span",null,o.email))),t.default.createElement(ge,null,t.default.createElement("h3",null,"Roles"),t.default.createElement("span",null,"Edit roles for this user"),t.default.createElement(he,null,t.default.createElement(C,{noBorder:!0},t.default.createElement("li",null,t.default.createElement("b",null,"Name")),t.default.createElement("li",null,t.default.createElement("b",null,"Action"))),t.default.createElement(C,null,t.default.createElement("li",null,"Job Title"),t.default.createElement("li",null,t.default.createElement("input",{name:"job_title",type:"text",value:d,onChange:s=>{u(s.target.value)}}))),t.default.createElement(C,null,t.default.createElement("li",null,"Role Title"),t.default.createElement("li",null,"Employee",o.is_admin&&", Admin")),t.default.createElement(C,null,t.default.createElement("li",null,"Admin"),t.default.createElement("li",null,t.default.createElement("input",{name:"is_admin",type:"checkbox",checked:p,onChange:s=>{l(s.target.checked)},disabled:o.is_admin})))),t.default.createElement(be,null,t.default.createElement(g,{category:$,onClick:r},"Cancel"),t.default.createElement(g,{category:D,type:"submit"},"Save")))),t.default.createElement("input",{type:"hidden",name:"employee_id",value:o.employee_id})))}G.propTypes={user:m.default.shape({employee_id:m.default.number,full_name:m.default.string,email:m.default.string,job_title:m.default.string,is_admin:m.default.bool,profile_picture:m.default.node,departments:m.default.shape([])}),onClose:m.default.func};G.defaultProps={user:{employee_id:"",full_name:"",email:"",job_title:"",is_admin:!1},onClose:()=>{}};var ye=G;function J({users:o,currentPage:r,totalPages:p,isLoading:l,size:d}){let[u,s]=(0,e.useState)(!1),[P,_]=(0,e.useState)({}),N=(0,e.useRef)(0),A=ae({currentPage:r===0?1:r+1,totalPages:p}),[,y]=U(),T=O();(0,e.useEffect)(()=>{T&&T.successMessage&&s(!1)},[o,T]);let w=a=>{u||_(a),s(!u)},I=a=>{y({size:d,page:a})},ke=()=>{y({size:d,page:r+1})},Pe=()=>{y({size:d,page:r-1})},_e=(a,B)=>e.default.createElement(v.Item,{key:B,onClick:()=>I(a),active:a===r},a),Ne=(()=>[...A.map((B,F)=>B===S?e.default.createElement(v.Ellipsis,{key:F}):_e(B,F))])(),H=()=>e.default.createElement("thead",null,e.default.createElement(se,null,e.default.createElement("th",null,"Name"),e.default.createElement("th",{className:"table-desktop-view"},"Email"),e.default.createElement("th",{className:"table-desktop-view"},"Job Title"),e.default.createElement("th",{className:"table-desktop-view"},"Roles"),e.default.createElement("th",null,"Action"))),Ae=a=>{y({size:a,page:r})};return!o.length&&!l?e.default.createElement(me,null,e.default.createElement("span",{className:"message"},"There are no results to show")):e.default.createElement("div",null,e.default.createElement(le,null,"Select the number of results",e.default.createElement("select",{ref:N,onChange:a=>Ae(a.target.value),defaultValue:d},e.default.createElement("option",{value:"5"},"5"),e.default.createElement("option",{value:"10"},"10"),e.default.createElement("option",{value:"15"},"15"),e.default.createElement("option",{value:"20"},"20"),e.default.createElement("option",{value:"25"},"25")),l?e.default.createElement("div",null,e.default.createElement(M,null,H()),e.default.createElement(ee,{src:ie,size:Z})):e.default.createElement(M,null,H(),e.default.createElement("tbody",null,o.map(a=>e.default.createElement(de,{key:a.employee_id},e.default.createElement("td",null,e.default.createElement("div",null,e.default.createElement("img",{src:a.profile_picture,alt:""}),a.full_name)),e.default.createElement("td",{className:"table-desktop-view"},a.email),e.default.createElement("td",{className:"table-desktop-view"},a.job_title),e.default.createElement("td",{className:"table-desktop-view"},"Employee",a.is_admin&&", Admin"),e.default.createElement("td",null,e.default.createElement(g,{category:D,onClick:()=>w(a),className:"row-btn"},"Edit roles")))))),!l&&e.default.createElement(pe,null,e.default.createElement("div",null,"Page"," ",r," ","of"," ",p),e.default.createElement(v,{boundarylinks:"true"},r>1&&e.default.createElement(v.Prev,{onClick:Pe}),Ne,r<p&&e.default.createElement(v.Next,{onClick:ke}))),u?e.default.createElement(ye,{user:P,onClose:()=>w()}):null))}J.propTypes={users:b.default.arrayOf(b.default.shape()),currentPage:b.default.number.isRequired,totalPages:b.default.number.isRequired,isLoading:b.default.bool.isRequired,size:b.default.number.isRequired};J.defaultProps={users:[]};var E=J;i();i();var Ce=f(z()),c=f(j());i();var we=n.div`
+  align-items: center;
+  display: flex;
+  background-color: #fff;
+  border: 1px solid #d3d3d4;
+  border-radius: 20px;
+  height: 40px;
+  justify-content: center;
+  max-width: 270px;
+  position: relative;
+  width: 100%;
+  transition: all 0.5s ease;
+`,Se=n.div`
+  left: 10px;
+  top: auto;
+  width: 22px;
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`,ve=n.input`
+  background-color: transparent;
+  border: none;
+  font-size: 14px;
+  height: 100%;
+  padding: 0 40px;
+  width: 100%;
+  &:focus {
+    outline: none;
+  }
+  ::placeholder,
+  ::-webkit-input-placeholder {
+    color: rgba(78, 81, 84, 0.65);
+    font-family: "Nunito", sans-serif;
+  }
+`,Te=n(W)`
+  font-size: 30px;
+  color: rgba(78, 81, 84, 0.65);
+`,je=n(R)`
+  font-size: 28px;
+  color: rgba(78, 81, 84, 0.65);
+  padding: 5px;
+  transition: all 0.3s ease-in-out;
+  &:hover {
+    color: #fff;
+  }
+`;function k({onSearch:o}){let[r,p]=(0,c.useState)("");return c.default.createElement(we,null,c.default.createElement(Se,null,c.default.createElement(Te,null)),c.default.createElement(ve,{type:"search",value:r,onChange:s=>{p(s.target.value)},placeholder:"Search for\u2026",onKeyDown:s=>{r&&s.key==="Enter"&&o(r)}}),r&&c.default.createElement(g,{type:"button",className:"clear-button",title:"Clear",category:X,onClick:()=>{p("")}},c.default.createElement(je,null)))}k.propTypes={onSearch:Ce.default.func.isRequired};function Ge(){let{users:o,totalPages:r,currentPage:p,size:l}=L(),[,d]=U();return x.default.createElement("div",null,x.default.createElement(te,null),x.default.createElement(ne,null,x.default.createElement("h2",null,"Admin page"),x.default.createElement(k,{onSearch:s=>{d({search:s})}})),x.default.createElement(E,{users:o,currentPage:p,totalPages:r,size:l}))}function Je(){return x.default.createElement(oe,null)}var Ee=Ge;export{Je as CatchBoundary,Ee as default};
