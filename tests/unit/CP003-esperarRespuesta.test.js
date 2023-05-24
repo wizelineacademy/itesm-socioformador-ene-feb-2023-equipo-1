a971116ece196fac2cf7678706aa8b72d5de92a4
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-24 12:45:04 -0600

    Update jest tests

--- tests/unit/CP003-esperarRespuesta.test.js
+++ tests/unit/CP003-esperarRespuesta.test.js
@@ -1,5 +1,7 @@
 import pdfConv from 'app/controllers/answerBot/pdfConv'
 
+jest.setTimeout(20000)
+
 describe('Testing pdfConv component', () => {
   it('Test question that should have expected answer', async () => {
     const data = await pdfConv([{ role: 'user', content: "What should I do if I test positive?" }])
--- tests/unit/CP004-continuarConvo.test.js
+++ tests/unit/CP004-continuarConvo.test.js
@@ -1,5 +1,7 @@
 import pdfConv from 'app/controllers/answerBot/pdfConv'
 
+jest.setTimeout(20000)
+
 describe('CP004 - Continuar conversación con el AnswerBot', () => {
   it('Se manda más de una pregunta y el AnswerBot puede seguir respondiendo', async () => {
     const data = await pdfConv([{ role: 'user', content: "What should I do if I test positive?" }])
