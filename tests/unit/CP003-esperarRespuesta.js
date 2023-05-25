3488f0d607cba19e43609e64a67c0a14e080c220
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-25 11:50:37 -0600

    Deleted redundant files

--- tests/unit/CP003-esperarRespuesta.js
+++
@@ -1,8 +0,0 @@
-import pdfConv from 'app/controllers/answerBot/pdfConv'
-
-describe('Testing pdfConv component', () => {
-  it('Test question that should have expected answer', async () => {
-    const data = await pdfConv([{ role: 'user', content: "What should I do if I test positive?" }])
-    expect(data.text).toBeTruthy();
-  });
-});
