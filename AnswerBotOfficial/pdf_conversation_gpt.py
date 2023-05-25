2a9900c1195b458f012a57776d226e6e44217b18
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-05-24 22:51:35 -0600

    OPENAI_API_KEY now stored in .env file

--- AnswerBotOfficial/pdf_conversation_gpt.py
+++ AnswerBotOfficial/pdf_conversation_gpt.py
@@ -1,6 +1,9 @@
+import os
 import re
+from pathlib import Path
 import fitz
 import openai
+from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from tqdm.auto import tqdm
@@ -74,7 +77,10 @@ def loadPDF(pdf_context, start_page=1):
 
 # ========================================= MAIN =========================================
 
-openai.api_key = "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"
+dotenv_path = Path('../.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 application = Flask(__name__)
 
