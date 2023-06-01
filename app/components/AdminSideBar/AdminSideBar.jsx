9877dcaee0a911d4cada56c33b8356fa7b4a1003
AnaPaulaPedroza <a00830553@itesm.mx>
2023-06-01 10:24:01 -0600

    correccion errores eslint pt2

--- app/components/AdminSideBar/AdminSideBar.jsx
+++ app/components/AdminSideBar/AdminSideBar.jsx
@@ -1,28 +1,27 @@
-import * as Styled from "app/components/AdminSideBar/AdminSideBar.Styled.jsx";
-import React, { useEffect, useState } from 'react';
+import * as Styled from 'app/components/AdminSideBar/AdminSideBar.Styled.jsx';
+import React from 'react';
 
 function SideBar({ departments, selectedDepartment, handleSelectDepartment }) {
+  return (
+    <Styled.MainContainer>
+      <Styled.DepContainer>
+        <Styled.TitleDep>Departaments</Styled.TitleDep>
 
-	return (
-		<Styled.MainContainer>
-			<Styled.DepContainer>
-				<Styled.TitleDep>Departaments</Styled.TitleDep>
-
-				<Styled.ListDep>
-					{departments.map((item) => (
-						<Styled.DepSelect
-							key={item.name}
-							aria-current={item.current ? "page" : undefined}
-							onClick={() => handleSelectDepartment(item.department_id)}
-							selected={item.department_id === selectedDepartment}
-							>
-							{item.name}
-						</Styled.DepSelect>
-					))}
-				</Styled.ListDep>
-			</Styled.DepContainer>
-		</Styled.MainContainer>
-	);
+        <Styled.ListDep>
+          {departments.map((item) => (
+            <Styled.DepSelect
+              key={item.name}
+              aria-current={item.current ? 'page' : undefined}
+              onClick={() => handleSelectDepartment(item.department_id)}
+              selected={item.department_id === selectedDepartment}
+            >
+              {item.name}
+            </Styled.DepSelect>
+          ))}
+        </Styled.ListDep>
+      </Styled.DepContainer>
+    </Styled.MainContainer>
+  );
 }
 
 export default SideBar;
