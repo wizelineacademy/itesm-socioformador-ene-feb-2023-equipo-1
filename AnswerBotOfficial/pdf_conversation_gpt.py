7480cdad86aa2a49e5267fe9c7cd6b35ed9fd2ff
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-06-12 09:09:09 -0600

    Update timeout times for tests

--- AnswerBotOfficial/pdf_conversation_gpt.py
+++ AnswerBotOfficial/pdf_conversation_gpt.py
@@ -240,6 +240,8 @@ def submit_conversation():
     conversation.append(answerStruct)
     return jsonify({'conversation': conversation, 'department': department})
 
+CORS(app)
+
 if __name__ == '__main__':
     initialize_index()
     app.run(host='0.0.0.0',port=4000, debug=True)
