85399398d8137687c21bf2789526dcacea7662eb
Patricio Santos <58954863+PatoSantos@users.noreply.github.com>
2023-06-13 08:42:47 -0600

    Fixed Cypress tests

--- cypress/e2e/Patricio Santos/CP023-continuarConversacion.cy.ts
+++ cypress/e2e/Patricio Santos/CP023-continuarConversacion.cy.ts
@@ -4,11 +4,11 @@ describe('CP023 - Continuar conversación con AnswerBot en la página principal'
     cy.get('#answerbotbutton').click() // Open the chat
     // Check that the welcome message is displayed correctly
     cy.get("#chatbotinput").type("What should I do if I test positive?").type('{enter}') // Send a question
-    cy.get('#msg-2', { timeout: 20000 })
+    cy.get('#msg-2', { timeout: 60000 })
       .should('not.have.text', '...')
       .should('not.have.text', '') // Wait for answer
     cy.get("#chatbotinput").type("And if I test negative?").type('{enter}') // Send a question
-    cy.get('#msg-4', { timeout: 20000 })
+    cy.get('#msg-4', { timeout: 60000 })
       .should('not.have.text', '...')
       .should('not.have.text', '') // Wait for answer
   })
