1a347ebd9256e0b9e66a3955e73d6e8b1d379fc3
AnaPaulaPedroza <a00830553@itesm.mx>
2023-06-15 09:40:20 -0600

    Update CPE2E-HacerPreguntaBotVerDashboard.cy.ts

--- cypress/e2e/Mauricio Cantu/CPE2E-HacerPreguntaBotVerDashboard.cy.ts
+++ cypress/e2e/Mauricio Cantu/CPE2E-HacerPreguntaBotVerDashboard.cy.ts
@@ -1,4 +1,4 @@
-describe('CP013 - Hacer pregunta a bot, postearla al foro y verla en dashboard', () => {
+describe('CPE2E - Hacer pregunta a bot, postearla al foro y verla en dashboard', () => {
     it('E2E', () => {
         cy.login('ana.pau1401@gmail.com', 'Aa00830553') // Login
         cy.get('#answerbotbutton')
