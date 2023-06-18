4743f24071789ef0cd853b50fd330a0bea5d8113
AnaPaulaPedroza <a00830553@itesm.mx>
2023-06-18 13:53:04 -0600

    pruebas e2e

---
+++ cypress/e2e/Ana Paula Pedroza/CPE2E-HacerPreguntaForoYVerlaEnDashboard.cy.ts
@@ -0,0 +1,18 @@
+describe('CPE2E - Hacer pregunta en foro y verla reflejada en dashboard', () => {
+    it('E2E', () => {
+        cy.login('ana.pau1401@gmail.com', 'Aa00830553') // Login
+        cy.contains('What should I do if I test positive?')
+        cy.get('#ask-button')
+            .click() // Ask Question
+        cy.get('.sc-jmnVvD > :nth-child(1) > .dropdown > .dropdown-toggle').click()
+        cy.get(':nth-child(1) > .dropdown > .dropdown-menu > :nth-child(1) > a').click()
+        cy.get('.editor-class').type("What should I do if I test positive test2?")
+        cy.get('#submit-btn').click()
+        cy.get('.eiJKJa').click()
+        cy.wait(3000)
+        cy.get('#dashboard').click() // Darle click al dashboard
+        cy.wait(3000)
+        cy.get('#dep-0').click() // Click a filtro All
+        cy.contains('What should I do if I test positive test2?')
+    })
+})
